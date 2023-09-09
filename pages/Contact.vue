3a5588961e820259c773616676a2cd53419da164
ekhan-odoogap <ekhan@odoogap.com>
2023-09-10 00:16:07 +0600

    main

---
+++ pages/Contact.vue
@@ -0,0 +1,14 @@
+<template>
+    <ContactForm />
+</template>
+
+<script setup>
+definePageMeta({
+    title: 'Contact-Us'
+})
+useHead({
+    link: [{ rel: 'canonical', href: 'https://vue-ecom.vercel.app/contact' }],
+});
+</script>
+
+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
